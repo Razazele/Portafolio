b0b3e6a7aab6043190cf3392665cb342a861b5f1
Razazele <pablo_verdugoj@hotmail.com>
2022-03-08 16:26:26 -0300

    hola

---
+++ src/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const  client = sanityClient({
+    projectId:'6cnpq42p',
+    dataset:'production',
+    apiVersion:'2022-02-01',
+    useCdn:true,
+    token:process.env.REACT_APP_SANITY_PROJECT_ID
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source)
